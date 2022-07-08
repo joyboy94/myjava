c4dcfea57ab67399849d2a2501740bf2e0777600
Malcolm Chokonte <princechonima@yahoo.fr>
2022-07-08 14:48:30 +0200

    premier commit

---
+++ ex1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ex1 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Quel est ton prénom ?\n");
+        String prenom = input.nextLine();
+
+        System.out.println("Quel est ton nom ? ");
+        String nom = input.nextLine();
+
+
+    }
+
+
+}
